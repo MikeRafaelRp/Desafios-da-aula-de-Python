1450a998b7eff51358fb3cd30af3c9419d85c988
Mike Rafael <mike.rdp@hotmail.com>
2023-03-31 01:15:28 -0300

    json

---
+++ desafios22.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome completo em maíusculo fica: {}, e em minúsculo fica: {}, ele tem ao todo {} letras'.format(nome.upper(),nome.lower(),len(nome) - nome.count(' ')))
